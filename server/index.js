4617395698e795f5e0bd7b15f3a18e4d4659bb64
Sasikant Biswal <biswalsashikant@gmail.com>
2023-05-16 01:29:57 +0530

    added register user api

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import path from "path";
 import {fileURLToPath} from "url";
 import connectDb from "./config/connectDb.js";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,8 +36,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage});
 
-connectDb();
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
+connectDb();
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 8080;
 
